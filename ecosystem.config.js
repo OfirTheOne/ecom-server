e6d4d7f7e073d81c5536f1e189032c34b4f0e06f
Ofir Genish <ofir3322@gmail.com>
2020-05-10 02:34:17 +0300

    ecosystem init

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    name: "ecomm--main-server",
+    script: '/dist/app/index.js',
+  }],
+
+  deploy : {
+    development : {
+      key : '/Users/ofirg/server-keys/ecomm-dev.pem',
+      user : 'ubuntu',
+      host : '104.198.192.76',
+      ref  : 'origin/develop',
+      repo : 'git@github.com:OfirTheOne/ecom-server.git',
+      path : '~/server',
+      'deploy-local': "mkdir -p logs && cp ~/.env . npm i && npm run build && pm2 reload ecosystem.json --env development",
+      'pre-post-deploy' : "echo 'Deploying source to servers'",
+      'pre-setup': ''
+    }
+  }
+};
