b756cbdf383ddb584304d86fc5be5076e1f526dd
Ofir Genish <ofir3322@gmail.com>
2020-05-11 02:35:20 +0300

    change commnd exec stage

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,15 +6,14 @@ module.exports = {
 
   deploy : {
     development : {
-      key : '/Users/ofirg/server-keys/ecomm-dev-new.pem',
+      key : '~/server-keys/ecomm-dev-new.pem',
       user : 'sa_103659762652968566694',
       host : '35.208.32.48',
       ref  : 'origin/develop',
       repo : 'git@github.com:OfirTheOne/ecom-server.git',
       path : '~/server',
-      'deploy-local': "mkdir -p logs && cp ~/.env . npm i && npm run build && pm2 reload ecosystem.json --env development",
-      'pre-post-deploy' : "echo 'Deploying source to servers'",
-      'pre-setup': ''
+      'post-deploy': "mkdir -p logs && cp ~/.env . npm i && npm run build && pm2 reload ecosystem.json --env development",
+      'pre-deploy-local' : "echo 'Deploying source to servers'",
     }
   }
 };
