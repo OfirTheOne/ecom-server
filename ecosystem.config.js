a5aa543b046f63d6dbfe77db83f13665eadf773a
Ofir Genish <ofir3322@gmail.com>
2020-05-11 02:40:45 +0300

    change remote deploy path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host : '35.208.32.48',
       ref  : 'origin/develop',
       repo : 'git@github.com:OfirTheOne/ecom-server.git',
-      path : '~/server',
+      path : '/home/sa_103659762652968566694/server',
       'post-deploy': "cp ~/.env . npm i && npm run build && pm2 reload ecosystem.json --env development",
       // 'post-deploy': "mkdir -p logs && cp ~/.env . npm i && npm run build && pm2 reload ecosystem.json --env development",
       'pre-deploy-local' : "echo 'Deploying source to servers'",
