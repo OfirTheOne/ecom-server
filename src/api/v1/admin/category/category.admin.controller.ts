5c3d2a24c01f0409dd4ee93088ddc1edce996026
Ofir Genish <ofir3322@gmail.com>
2020-05-27 15:26:14 +0300

    extands public controller;

--- src/api/v1/admin/category/category.admin.controller.ts
+++ src/api/v1/admin/category/category.admin.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post } from '@o-galaxy/ether/core';
 import { Request, Response, NextFunction } from 'express';
 import { CategoryAdminHandler } from './category.admin.handler';
+import { CategoryController } from '../../client/category/category.controller';
 
 @Controller({ path: '/category' })
-export class CategoryAdminController {
+export class CategoryAdminController extends CategoryController {
 
 
-    constructor(private categoryAdminHandler: CategoryAdminHandler) { }
+    constructor(private categoryAdminHandler: CategoryAdminHandler) { 
+        super(categoryAdminHandler)
+    }
 
     @Post()
     public async createCategory(req: Request, res: Response, next: NextFunction) {
@@ -19,6 +22,27 @@ export class CategoryAdminController {
             return next(error);
         }
     }
+    /*
+    @Get('/:id')
+    public async getCategoryById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = req.params.id;
+            const result = await this.categoryAdminHandler.getCategoryById(id);
+            return res.send(result);
+        } catch (error) {
+            return next(error);
+        }
+    }
 
+    @Get()
+    public async getAllItems(req: Request, res: Response, next: NextFunction) {
+        try {
+            const result = await this.categoryAdminHandler.getAllCategories();
+            return res.send(result);
+        } catch (error) {
+            return next(error);
+        }
+    }
+    */
 
 }
